78b74ce29796895cbed729e75a9c8e30aa6ad9af
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-09-15 17:39:48 +0530

    Added ability to update a task

--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -9,6 +9,11 @@ const create = payload =>
     task: payload,
   });
 
-const tasksApi = { list, show, create };
+const update = ({ slug, payload }) =>
+  axios.put(`/tasks/${slug}`, {
+    task: payload,
+  });
+
+const tasksApi = { list, show, create, update };
 
 export default tasksApi;
