eaf3f4d7dfa6ed1c91d49ee4e97b302fbe8d86f5
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-09-15 16:06:56 +0530

    Implemented create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :tasks, only: :index, param: :slug
+  resources :tasks, only: %i[index create], param: :slug
 
   # Defines the root path route ("/")
   # root "articles#index"
