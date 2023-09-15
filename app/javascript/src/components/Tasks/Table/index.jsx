f00a4a017c24b9039b8f4d99a2965a00e817e020
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-09-15 17:07:01 +0530

    Added show page for a task

--- app/javascript/src/components/Tasks/Table/index.jsx
+++ app/javascript/src/components/Tasks/Table/index.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import Header from "./Header";
 import Row from "./Row";
 
-const Table = ({ data }) => (
+const Table = ({ data, showTask }) => (
   <div className="flex flex-col mt-10 ">
     <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
         <div className="overflow-hidden border-b border-gray-200 shadow md:custom-box-shadow">
           <table className="min-w-full divide-y divide-gray-200">
             <Header />
-            <Row data={data} />
+            <Row data={data} showTask={showTask} />
           </table>
         </div>
       </div>
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :tasks, only: %i[index create], param: :slug
+  resources :tasks, only: %i[index create show], param: :slug
 
   # Defines the root path route ("/")
   # root "articles#index"
