d2d4bf152edd132795b3515b4d9724b8634d8667
Lakshya Kanungo <lakshya.kanungo@bigbinary.com>
2023-09-17 18:33:02 +0530

    Added starring feature to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,12 +6,32 @@ class TasksController < ApplicationController
   before_action :load_task!, only: %i[show update destroy]
   before_action :ensure_authorized_update_to_restricted_attrs, only: :update
 
+=begin
   def index
     tasks = policy_scope(Task)
     # @pending_tasks = tasks.pending.as_json(include: { assigned_user: { only: %i[name id] } })
     @pending_tasks = tasks.pending.includes(:assigned_user)
     @completed_tasks = tasks.completed
     # render_json({ tasks: tasks_with_assigned_user })
+  end
+
+  def index
+    tasks = policy_scope(Task)
+
+    pending_starred = tasks.pending.starred.includes(:assigned_user).order('updated_at DESC')
+    pending_unstarred = tasks.pending.unstarred.includes(:assigned_user).order('updated_at DESC')
+    @pending_tasks = pending_starred + pending_unstarred
+
+    completed_starred = tasks.completed.starred.order('updated_at DESC')
+    completed_unstarred = tasks.completed.unstarred.order('updated_at DESC')
+    @completed_tasks = completed_starred + completed_unstarred
+  end
+=end
+
+  def index
+    tasks = policy_scope(Task)
+    @pending_tasks = tasks.includes(:assigned_user).of_status(:pending)
+    @completed_tasks = tasks.of_status(:completed)
   end
 
   def create
@@ -47,7 +67,7 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:title, :assigned_user_id, :progress)
+      params.require(:task).permit(:title, :assigned_user_id, :progress, :status)
     end
 
     def ensure_authorized_update_to_restricted_attrs
